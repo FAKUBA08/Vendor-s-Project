6bce428a9240509207ee37218f6728f0dad7910b
FAKUBA08 <badmusfaruq01@gmail.com>
2024-10-19 15:45:58 +0100

    Update signup URL to point to deployed backend

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -8,12 +8,11 @@ const handleSubmit = async (e) => {
         setFormErrors(errors);
     } else {
         try {
-            const response = await axios.post('http://localhost:3000/api/auth/signup', formData);
+            const response = await axios.post('https://vendors-node.vercel.app/api/auth/signup', formData); // Update this URL
             console.log('User created:', response.data);
        
         } catch (error) {
             console.error('Error signing up:', error.response.data);
-            
         }
     }
 };
