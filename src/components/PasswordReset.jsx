4edfa933dd47ee21c5d9016d70d66cbf3706b08b
FAKUBA08 <badmusfaruq01@gmail.com>
2024-10-21 10:18:11 +0100

    all

--- src/components/PasswordReset.jsx
+++ src/components/PasswordReset.jsx
@@ -26,7 +26,7 @@ const ResetPassword = () => {
         setLoading(true); // Start loading
 
         try {
-            const response = await fetch(`https://your-backend-app.vercel.app/api/auth/reset-password/${token}`, {
+            const response = await fetch(`http://localhost:3000/api/auth/reset-password/${token}`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
